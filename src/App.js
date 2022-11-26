d21c581280f050253c2ea00fbccc0f0d803de4bc
UBC Student <burtonm1@students.cs.ubc.ca>
2022-11-25 17:36:27 -0800

    nav bar styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 import Footer from './containers/footer/Footer';
-import Blog from './containers/blog/Blog';
+import Contact from './containers/contact/Contact';
 import Features from './containers/features/Features';
 import Header from './containers/header/Header';
-import Possibility from './containers/possibility/Possibility';
-import WhatGPT3 from './containers/whatGPT3/WhatGPT3';
 import CTA from './components/cta/CTA';
 import Navbar from './components/navbar/Navbar';
 import Brand from './components/brand/Brand';
@@ -20,11 +18,9 @@ const App = () => {
         <Header />
       </div>
       <Brand></Brand>
-      <WhatGPT3></WhatGPT3>
       <Features></Features>
-      <Possibility></Possibility>
       <CTA></CTA>
-      <Blog></Blog>
+      <Contact></Contact>
       <Footer></Footer>
     </div>
   )
