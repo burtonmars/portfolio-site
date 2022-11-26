7ba28a7906a35f2959e336a6779b7cf6bd797c8d
UBC Student <burtonm1@students.cs.ubc.ca>
2022-11-25 16:03:49 -0800

    restart init commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React from 'react';
+
+import Footer from './containers/footer/Footer';
+import Blog from './containers/blog/Blog';
+import Features from './containers/features/Features';
+import Header from './containers/header/Header';
+import Possibility from './containers/possibility/Possibility';
+import WhatGPT3 from './containers/whatGPT3/WhatGPT3';
+import CTA from './components/cta/CTA';
+import Navbar from './components/navbar/Navbar';
+import Brand from './components/brand/Brand';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>My new Site</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand></Brand>
+      <WhatGPT3></WhatGPT3>
+      <Features></Features>
+      <Possibility></Possibility>
+      <CTA></CTA>
+      <Blog></Blog>
+      <Footer></Footer>
     </div>
-  );
-}
+  )
+};
 
 export default App;
